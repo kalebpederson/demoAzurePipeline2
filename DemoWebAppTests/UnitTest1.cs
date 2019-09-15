513cd800af97d5b1e80f01bc5dfe0a33ffec9550
Kaleb Pederson <kaleb.pederson@gmail.com>
2019-09-14 21:57:01 -0600

    Add unit tests

---
+++ DemoWebAppTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace DemoWebAppTests
+{
+  public class Tests
+  {
+    [Test]
+    public void Test1()
+    {
+      Assert.Pass();
+    }
+    
+    [Test]
+    public void Test2()
+    {
+      Assert.Pass();
+    }
+    
+  }
+}
